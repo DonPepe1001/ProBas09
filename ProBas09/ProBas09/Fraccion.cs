2149a77dcdabff6f454ff579667033fca5d03466
DonPepe1001 <emanuel.rendon7@gmail.com>
2022-03-02 22:00:56 -0600

    Creacion de el proyecto

---
+++ ProBas09/ProBas09/Fraccion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBas08
+{
+    public class Fraccion
+    {
+        #region propiedades
+        public int Numerador { get; set; }
+        public int Denominador { get; set; }
+        #endregion
+        #region métodos
+        public Fraccion()
+        {
+            this.Numerador = 0;
+            this.Denominador = 1;
+
+        }
+        public Fraccion(int numerador, int denominador)
+        {
+            this.Numerador = numerador;
+            this.Denominador = denominador;
+        }
+        public Fraccion multiplica(Fraccion value)
+        {
+            return new Fraccion(this.Numerador * value.Numerador, this.Denominador * value.Denominador);
+        }
+        public string imprimir()
+        {
+            return $"{this.Numerador}/{this.Denominador}";
+        }
+        #endregion
+
+    }
+}
---
+++ ProBas09/ProBas09/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBas08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraccion A = new Fraccion(1, 2);
+            Fraccion B = new Fraccion(3, 4);
+            Fraccion C = new Fraccion();
+            Console.WriteLine($"A:{A.imprimir()}");
+            Console.WriteLine($"B:{B.imprimir()}");
+            C = A.multiplica(C);
+            Console.WriteLine($"La multiplicación de A*B es:{C}");
+            Console.WriteLine($"Multiplicación de A*B:{A.multiplica(B).imprimir()}");
+            Console.ReadLine();
+        }
+    }
+}
