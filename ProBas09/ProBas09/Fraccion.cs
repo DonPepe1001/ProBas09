8dbcb495c72a17636606f8f772ebf08223f48fa2
DonPepe1001 <emanuel.rendon7@gmail.com>
2022-03-07 23:30:24 -0600

    Menu incluuido

--- ProBas09/ProBas09/Fraccion.cs
+++ ProBas09/ProBas09/Fraccion.cs
@@ -4,35 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProBas08
+namespace ProBas09
 {
-    public class Fraccion
+    public class Program
     {
-        #region propiedades
-        public int Numerador { get; set; }
-        public int Denominador { get; set; }
-        #endregion
-        #region métodos
-        public Fraccion()
+        static void Main(string[] args)
         {
-            this.Numerador = 0;
-            this.Denominador = 1;
-
+            int opcion = 0;
+            while (opcion != 5)
+            {
+                Console.WriteLine("Elige Operacion");
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("3. Multiplicacion");
+                Console.WriteLine("4. Division");
+                opcion = int.Parse(Console.ReadLine());
+                Fraccion A = new Fraccion("A");
+                Console.WriteLine("Escriba Numerador de A");
+                A.Numerador = int.Parse(Console.ReadLine());
+                A.Denominador = LeerDenominador("A");
+                Fraccion B = new Fraccion("B");
+                Console.WriteLine("Escriba Numerador de B");
+                B.Numerador = int.Parse(Console.ReadLine());
+                B.Denominador = LeerDenominador("B");
+                Console.Clear();
+                Console.WriteLine(A.ToString());
+                Console.WriteLine(B.ToString());
+                switch (opcion)
+                {
+                    case 1:
+                        Fraccion C = A + B;
+                        C.NombreOperando = "A + B";
+                        Console.WriteLine(C.ToString());
+                        break;
+                    case 2:
+                        Fraccion D = A - B;
+                        D.NombreOperando = "A - B";
+                        Console.WriteLine(D.ToString());
+                        break;
+                    case 3:
+                        Fraccion E = A * B;
+                        E.NombreOperando = "A * B";
+                        Console.WriteLine(E.ToString());
+                        break;
+                    case 4:
+                        Fraccion F = A / B;
+                        F.NombreOperando = "A / B";
+                        Console.WriteLine(F.ToString());
+                        break;
+                }
+            }
         }
-        public Fraccion(int numerador, int denominador)
+        public static int LeerDenominador(string operando)
         {
-            this.Numerador = numerador;
-            this.Denominador = denominador;
+            int aux = 0;
+            while (aux == 0)
+            {
+                Console.WriteLine($"Escriba denominador de {operando} diferente a 0");
+                aux = int.Parse(Console.ReadLine());
+            }
+            return aux;
         }
-        public Fraccion multiplica(Fraccion value)
-        {
-            return new Fraccion(this.Numerador * value.Numerador, this.Denominador * value.Denominador);
-        }
-        public string imprimir()
-        {
-            return $"{this.Numerador}/{this.Denominador}";
-        }
-        #endregion
-
     }
 }
+            
--- ProBas09/ProBas09/Program.cs
+++ ProBas09/ProBas09/Program.cs
@@ -4,21 +4,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProBas08
+namespace ProBas09
 {
-    internal class Program
+    public class Fraccion
     {
-        static void Main(string[] args)
+        public string NombreOperando { get; set; }
+        public int Numerador { get; set; }
+        public int Denominador { get; set; }
+        public Fraccion()
         {
-            Fraccion A = new Fraccion(1, 2);
-            Fraccion B = new Fraccion(3, 4);
-            Fraccion C = new Fraccion();
-            Console.WriteLine($"A:{A.imprimir()}");
-            Console.WriteLine($"B:{B.imprimir()}");
-            C = A.multiplica(C);
-            Console.WriteLine($"La multiplicación de A*B es:{C}");
-            Console.WriteLine($"Multiplicación de A*B:{A.multiplica(B).imprimir()}");
-            Console.ReadLine();
+            this.NombreOperando = "a";
+            this.Numerador = 3;
+            this.Denominador = 6;
+        }
+        public Fraccion(int numerador, int denominador)
+        {
+            this.NombreOperando = "A";
+            this.Numerador = numerador;
+            this.Denominador = denominador;
+        }
+        public Fraccion(string nombreOperador, int numerador, int denominador)
+        {
+            this.NombreOperando = nombreOperador;
+            this.Numerador = numerador;
+            this.Denominador = denominador;
+        }
+        public Fraccion(string NombreOperando)
+        {
+            this.NombreOperando = NombreOperando;
+        }
+        public static Fraccion operator *(Fraccion A, Fraccion B)
+        {
+            return new Fraccion(A.Numerador * B.Numerador, A.Denominador * B.Denominador);
+        }
+        public static Fraccion operator /(Fraccion A, Fraccion B)
+        {
+            return new Fraccion(A.Numerador * B.Denominador, A.Denominador * B.Numerador);
+        }
+        public static Fraccion operator +(Fraccion A, Fraccion B)
+        {
+            return new Fraccion(A.Numerador * B.Denominador + A.Denominador * B.Numerador, A.Denominador * B.Denominador);
+        }
+        public static Fraccion operator -(Fraccion A, Fraccion B)
+        {
+            return new Fraccion(A.Numerador * B.Denominador - A.Denominador * B.Numerador, A.Denominador * B.Denominador);
+        }
+        public override string ToString()
+        {
+            this.simplificar();
+            return $"{this.NombreOperando} = {this.Numerador} / {this.Denominador}";
+        }
+        public void simplificar()
+        {
+            int num = this.Numerador;
+            int den = this.Denominador;
+            int mcd = 0;
+            MCD(ref num, ref den, ref mcd);
+        }
+        static void MCD(ref int num, ref int den, ref int mcd)
+        {
+            for (int x = 1; x <= den; x++)
+            {
+                if ((num % x == 0) && (den % x == 0))
+                    mcd = x;
+            }
+            if (mcd == 0)
+            {
+                return;
+            }
+            else
+            {
+                num = num / mcd;
+                den = den / mcd;
+            }
         }
     }
 }
